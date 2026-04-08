f74a43ee5d61f4ca7d01612362cc8e578589f6a3
Niclas <niclas.rogulski@gmail.com>
2026-04-09 00:19:53 +1000

    chore: remove unused deps and css-only tooling baggage

--- stylelint.config.js
+++ stylelint.config.js
@@ -1,24 +1,7 @@
 export default {
     plugins: ['stylelint-prettier'],
-    extends: [
-        'stylelint-config-standard',
-        'stylelint-config-tailwindcss',
-        'stylelint-config-standard-scss',
-    ],
+    extends: ['stylelint-config-standard', 'stylelint-config-tailwindcss'],
     rules: {
-        'scss/at-rule-no-unknown': [
-            true,
-            {
-                ignoreAtRules: [
-                    'tailwind',
-                    'apply',
-                    'variants',
-                    'responsive',
-                    'screen',
-                    'theme',
-                ],
-            },
-        ],
         'selector-class-pattern': [
             /^(rt-.*|[a-z0-9-]+)$/,
             {
